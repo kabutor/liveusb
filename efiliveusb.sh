f016e5a841fa0c13d70f0621cea1064a2ed408b5
kabutor <43006263+kabutor@users.noreply.github.com>
2023-01-14 13:18:28 +0100

    v2

--- efiliveusb.sh
+++ efiliveusb.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
-
+# change to debian if you want to use a debian distribution
+distribution="ubuntu"
 
 lsblk 2>/dev/null
 echo "Type your pendrive unit (ex: /dev/sdg /dev/sdc not the partition)"
@@ -62,12 +63,12 @@ mount $boot_unit $temp_mnt
 # grub-install creates EFI/Boot directories, use lowercase in advance
 mkdir -p ${temp_mnt}/efi/boot
 grub-install --target=x86_64-efi --removable --efi-directory=$temp_mnt --boot-directory=$temp_mnt 
-mkdir ${temp_mnt}/efi/debian
-cp --preserve=timestamps grub_efi.cfg ${temp_mnt}/efi/debian/grub.cfg
+mkdir ${temp_mnt}/efi/${distribution}
+cp --preserve=timestamps grub_efi.cfg ${temp_mnt}/efi/${distribution}/grub.cfg
 
 cp shimx64.efi ${temp_mnt}/efi/boot/bootx64.efi
-cp grubx64.efi.signed ${temp_mnt}/efi/boot/grubx64.efi
-cp grubenv ${temp_mnt}/efi/debian/
+cp grubx64.efi.${distribution}.signed ${temp_mnt}/efi/boot/grubx64.efi
+cp grubenv ${temp_mnt}/efi/${distribution}/
 
 wget -nv "https://boot.ipxe.org/ipxe.efi" -O ${temp_mnt}/efi/ipxe.efi
 wget -nv "https://github.com/tianocore/edk/raw/master/Other/Maintained/Application/UefiShell/bin/x64/Shell.efi" -O ${temp_mnt}/efi/shellx64.efi
@@ -75,7 +76,7 @@ wget -nv "https://github.com/tianocore/edk/raw/master/Other/Maintained/Applicati
 umount $temp_mnt 
 
 mount $isos_unit $temp_mnt
-mkdir -p ${temp_mnt}/ubuntu ${temp_mnt}/debian ${temp_mnt}/clonezilla
+mkdir -p ${temp_mnt}/ubuntu ${temp_mnt}/${distribution} ${temp_mnt}/clonezilla
 umount $temp_mnt
 rmdir $temp_mnt
 echo "Success"
